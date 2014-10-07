825de8e19bb705b8db1dca76857b7877b5cc2fdd
Mariia Shevchenko <mashkovsky@bigmir.net>
2014-10-07 21:53:23 +0200

    Created repository exception.

---
+++ src/main/java/cz/muni/fi/pa165/airport/exception/RepositoryException.java
@@ -0,0 +1,11 @@
+package cz.muni.fi.pa165.airport.exception;
+
+/**
+ * Exception thrown whenever any of storage exception occurs. E.G. when removing entity which
+ * is linked to another etc.
+ *
+ * @author Mariia Shevchenko
+ */
+public class RepositoryException extends RuntimeException {
+
+}
