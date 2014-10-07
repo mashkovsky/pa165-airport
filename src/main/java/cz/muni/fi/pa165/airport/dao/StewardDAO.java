6aba359a6426381ca67d5efad262b96025e2beed
Mariia Shevchenko <mashkovsky@bigmir.net>
2014-10-07 23:31:48 +0200

    Implemented StewardDAO.

--- src/main/java/cz/muni/fi/pa165/airport/dao/StewardDAO.java
+++ src/main/java/cz/muni/fi/pa165/airport/dao/StewardDAO.java
@@ -1,11 +1,13 @@
 package cz.muni.fi.pa165.airport.dao;
 
 import cz.muni.fi.pa165.airport.entity.Steward;
+import cz.muni.fi.pa165.airport.exception.RepositoryException;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.List;
 
 /**
@@ -21,26 +23,62 @@ public class StewardDAO implements IStewardDAO {
 
     @Override
     public void create(Steward steward) {
+        if (steward == null) {
+            throw new IllegalArgumentException("Steward is null");
+        }
+        if (steward.getId() != null) {
+            throw new IllegalArgumentException("Steward id is not null.");
+        }
+        if (steward.getFirstName() == null) {
+            throw new IllegalArgumentException("Steward first name is null.");
+        }
+        if (steward.getLastName() == null) {
+            throw new IllegalArgumentException("Steward last name is null.");
+        }
 
+        em.persist(steward);
     }
 
     @Override
     public void update(Steward steward) {
+        if (steward == null) {
+            throw new IllegalArgumentException("Steward is null");
+        }
+        if (steward.getId() == null) {
+            throw new IllegalArgumentException("Steward id is null.");
+        }
+        if (steward.getFirstName() == null) {
+            throw new IllegalArgumentException("Steward first name is null.");
+        }
+        if (steward.getLastName() == null) {
+            throw new IllegalArgumentException("Steward last name is null.");
+        }
 
+        em.merge(steward);
     }
 
     @Override
     public void delete(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("ID is null");
+        }
 
+        Steward steward = em.find(Steward.class, id);
+        if (steward == null) {
+            throw new RepositoryException("Steward with ID " + id + "does not exist");
+        }
+
+        em.remove(steward);
     }
 
     @Override
     public Steward find(Long id) {
-        return null;
+        return em.find(Steward.class, id);
     }
 
     @Override
     public List<Steward> getAll() {
-        return null;
+        Query query = em.createQuery("SELECT s FROM Steward s ORDER BY s.lastName, s.firstName");
+        return (List<Steward>) query.getResultList();
     }
 }
