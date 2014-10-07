2243ba75c77fd8670d659e5ab2db4189b0fc8d36
Mariia Shevchenko <mashkovsky@bigmir.net>
2014-10-07 22:46:08 +0200

    Implemented steward entity.

--- src/main/java/cz/muni/fi/pa165/airport/entity/Steward.java
+++ src/main/java/cz/muni/fi/pa165/airport/entity/Steward.java
@@ -1,4 +1,52 @@
 package cz.muni.fi.pa165.airport.entity;
 
+/**
+ * @author Mariia Shevchenko
+ */
 public class Steward {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Steward)) return false;
+
+        Steward steward = (Steward) o;
+
+        if (id != null ? !id.equals(steward.id) : steward.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
