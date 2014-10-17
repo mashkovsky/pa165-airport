b3e66040e5342fbe36c9d4d7db2cc79f8a1425f2
Mariia Shevchenko <mashkovsky@bigmir.net>
2014-10-17 15:30:35 +0200

    Inject dozer converter into service.

--- src/test/java/cz/muni/fi/pa165/airport/service/BaseServiceTest.java
+++ src/test/java/cz/muni/fi/pa165/airport/service/BaseServiceTest.java
@@ -1,9 +1,13 @@
 package cz.muni.fi.pa165.airport.service;
 
+import org.dozer.DozerBeanMapper;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Base test case for service classes using mockito runner with annotations
@@ -16,5 +20,17 @@ public abstract class BaseServiceTest {
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+    }
+
+    /**
+     * Inject dozer mapper into conversion aware service
+     * @param service service to inject dozer to
+     * @param <T>
+     */
+    protected <T extends ConversionAware> void injectDozer(T service) {
+        List<String> mappingFiles = new ArrayList<String>();
+        mappingFiles.add("mapping/global.xml");
+
+        service.setMapper(new DozerBeanMapper(mappingFiles));
     }
 }
