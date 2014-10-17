853a376f50f2445f19561d5d52700096a8b8a8ec
Mariia Shevchenko <mashkovsky@bigmir.net>
2014-10-17 13:29:17 +0200

    Introduce BaseDTO.

---
+++ src/main/java/cz/muni/fi/pa165/airport/service/dto/BaseDTO.java
@@ -0,0 +1,22 @@
+package cz.muni.fi.pa165.airport.service.dto;
+
+import java.io.Serializable;
+
+/**
+ * Common class for all DTO objects. Will ensure that DTO is serializable and has ID attribute
+ *
+ * @author Mariia Schevchenko
+ */
+public abstract class BaseDTO implements Serializable {
+
+    private Long id;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
