ae4e194eb34fcc7cde2e918d1b08be27bc4f0e91
Mariia Shevchenko <mashkovsky@bigmir.net>
2014-10-17 13:33:59 +0200

    Flight service interface.

---
+++ src/main/java/cz/muni/fi/pa165/airport/service/IFlightService.java
@@ -0,0 +1,54 @@
+package cz.muni.fi.pa165.airport.service;
+
+import cz.muni.fi.pa165.airport.service.dto.FlightDetailDTO;
+import cz.muni.fi.pa165.airport.service.dto.FlightMinimalDTO;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @author Mariia Shevchenko
+ */
+@Transactional
+public interface IFlightService {
+
+    /**
+     * Create new flight
+     *
+     * @param flight flight to create
+     * @throws IllegalStateException    if plane or steward is not available for flight duration
+     *                                  if departure date is in future according to arrival (past arrival)
+     */
+    void createFlight(FlightDetailDTO flight);
+
+    /**
+     * Update existing flight
+     *
+     * @param flight flight to create
+     * @throws IllegalStateException    if plane or steward is not available for flight duration
+     *                                  if departure date is in future according to arrival (past arrival)
+     */
+    void updateFlight(FlightDetailDTO flight);
+
+    /**
+     * Delete flight identified by {@code id} from system.
+     *
+     * @param flightId flight unique ID
+     */
+    void deleteFlight(Long flightId);
+
+    /**
+     * Get all flights ordered by departure date
+     *
+     * @return list of all flights or empty list if no flight exist
+     */
+    List<FlightMinimalDTO> getAllFlights();
+
+    /**
+     * Get flight detail by {@code flightId}
+     *
+     * @param flightId unique flight ID
+     * @return flight detail or {@code null} if no such flight exist
+     */
+    FlightDetailDTO getFlightDetail(Long flightId);
+}
