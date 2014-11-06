5b78b087e94741b63dfa1d3cea7b1afe735c017a
Jan Jílek <jan.jilek@freshflow.cz>
2014-11-06 12:27:36 +0100

    Removed comment template

--- airport-service/src/main/java/cz/muni/fi/pa165/airport/service/service/PlaneService.java
+++ airport-service/src/main/java/cz/muni/fi/pa165/airport/service/service/PlaneService.java
@@ -1,9 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pa165.airport.service.service;
-
 import cz.muni.fi.pa165.airport.dao.dao.IPlaneDAO;
 import cz.muni.fi.pa165.airport.dao.entity.Plane;
 import cz.muni.fi.pa165.airport.service.service.dto.PlaneDTO;
--- airport-service/src/test/java/cz/muni/fi/pa165/airport/service/PlaneServiceTest.java
+++ airport-service/src/test/java/cz/muni/fi/pa165/airport/service/PlaneServiceTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pa165.airport.service;
 
 import cz.muni.fi.pa165.airport.dao.dao.IPlaneDAO;
