c15cfe448710e02196eea1bc681d833cbc67ba98
Matej Chrenko <info@kodujeme.sk>
2014-11-07 20:17:51 +0100

    steward service test

---
+++ airport-service/src/test/java/cz/muni/fi/pa165/airport/service/StewardServiceTest.java
@@ -0,0 +1,143 @@
+package cz.muni.fi.pa165.airport.service;
+
+import cz.muni.fi.pa165.airport.dao.dao.IStewardDAO;
+import cz.muni.fi.pa165.airport.dao.entity.Steward;
+import cz.muni.fi.pa165.airport.service.service.StewardService;
+import cz.muni.fi.pa165.airport.service.service.dto.StewardDTO;
+import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.mockito.Mock;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Matej Chrenko
+ */
+public class StewardServiceTest extends BaseServiceTest {
+
+    @Mock
+    private IStewardDAO stewardDAO;
+
+    @InjectMocks
+    private StewardService stewardService;
+
+    @Before
+    @Override
+    public void setUp() {
+        super.setUp();
+        stewardService.setMapper(mapper);
+    }
+
+    @Test
+    public void testGetAllEmpty() {
+        when(stewardDAO.getAll()).thenReturn(new ArrayList<Steward>());
+        assertTrue(stewardService.getAllStewards().isEmpty());
+    }
+
+    /*
+     * Create tests
+     */
+    @Test
+    public void testCreate() {
+        Steward steward = ServiceTestHelper.prepareSteward(null, "Bob", "Dylan");
+        StewardDTO dto = mapper.map(steward, StewardDTO.class);
+
+        stewardService.createSteward(dto);
+        ArgumentCaptor<Steward> argument = ArgumentCaptor.forClass(Steward.class);
+        verify(stewardDAO).create(argument.capture());
+        AssertTestHelper.assertDeepEqualSteward(argument.getValue(), dto);
+    }
+
+    @Test
+    public void testCreateNotNullId() {
+        Steward steward = ServiceTestHelper.prepareSteward(1L, "Bob", "Dylan");
+
+        StewardDTO dto = mapper.map(steward, StewardDTO.class);
+
+        try {
+            stewardService.createSteward(dto);
+            fail();
+        } catch (IllegalArgumentException e) {
+            // OK
+        }
+
+        verify(stewardDAO, times(0)).create(any(Steward.class));
+    }
+
+    /*
+     * Update tests
+     */
+    @Test
+    public void testUpdate() {
+        Steward steward = ServiceTestHelper.prepareSteward(1L, "Bob", "Praha");
+        StewardDTO dto = mapper.map(steward, StewardDTO.class);
+
+        stewardService.updateSteward(dto);
+
+        ArgumentCaptor<Steward> argument = ArgumentCaptor.forClass(Steward.class);
+        verify(stewardDAO).update(argument.capture());
+
+        AssertTestHelper.assertDeepEqualSteward(argument.getValue(), dto);
+    }
+
+    @Test
+    public void testUpdateNullId() {
+        Steward steward = ServiceTestHelper.prepareSteward(null, "Bob", "Ostrava");
+        StewardDTO dto = mapper.map(steward, StewardDTO.class);
+
+        try {
+            stewardService.updateSteward(dto);
+            fail();
+        } catch (IllegalArgumentException e) {
+            // OK
+        }
+
+        verify(stewardDAO, times(0)).create(any(Steward.class));
+    }
+
+    /*
+     *  Delete tests
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testDeleteNullID() {
+        stewardService.deleteSteward(null);
+    }
+
+    @Test
+    public void testDelete() {
+        stewardService.deleteSteward(1L);
+
+        ArgumentCaptor<Long> argument = ArgumentCaptor.forClass(Long.class);
+        verify(stewardDAO).delete(argument.capture());
+
+        assertEquals(argument.getValue(), Long.valueOf(1));
+    }
+
+    /*
+     * Test get steward
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetStewardNullId() {
+        stewardService.getSteward(null);
+    }
+
+    @Test
+    public void testGetSteward() {
+        Steward steward = ServiceTestHelper.prepareSteward(1L, "Bob", "Dylan");
+        when(stewardDAO.find(steward.getId())).thenReturn(steward);
+        StewardDTO detail = stewardService.getSteward(steward.getId());
+
+        AssertTestHelper.assertDeepEqualSteward(steward, detail);
+    }
+
+}
