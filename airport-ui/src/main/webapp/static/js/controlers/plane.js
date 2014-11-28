bd02e00758ef9d417450629e851e0e36f55586cd
Zdenek <>
2014-11-28 22:16:06 +0100

    Added communication with server.

--- airport-ui/src/main/webapp/static/js/controlers/plane.js
+++ airport-ui/src/main/webapp/static/js/controlers/plane.js
@@ -1,9 +1,62 @@
 function PlaneController($scope, $http, apiProvider, $routeParams, $translate, $timeout) {
+    
     if($routeParams.planeId) {
         $scope.plane = apiProvider.plane().get({id: $routeParams.planeId});
     } else {
         $scope.planes = apiProvider.plane().query();
     }
+    
+    // update plane
+    $scope.updatePlane = function() {
+        //
+        apiProvider.plane().update({
+            id: $routeParams.planeId,
+            type: $scope.plane.type,
+            name: $scope.plane.name,
+            capacity: $scope.plane.capacity
+        },
+        function(res) {
+            if(res.$resolved) {
+                $scope.result = 'ok';
+            } 
+            else {
+                $scope.result = 'error';
+            }
+        });
+    }
+    
+    $scope.createPlane = function() {
+        apiProvider.plane().save({
+            type: $scope.plane.type,
+            name: $scope.plane.name,
+            capacity: $scope.plane.capacity
+        },
+        function(res) {
+            if(res.$resolved) {
+                $scope.result = 'ok';
+            } 
+            else {
+                $scope.result = 'error';
+            }
+        });
+    }
+    
+    $scope.deletePlane = function() {
+        //
+        apiProvider.plane().remove({
+            id: $routeParams.planeId
+        },
+        function(res) {
+            if(res.$resolved) {
+                $scope.result = 'ok';
+            } 
+            else {
+                $scope.result = 'error';
+            }
+        });
+    }
+    
+    ///////////////////////////////////////////////////////////////////
 }
 
 PlaneController.$inject = ['$scope', '$http', 'apiProvider', '$routeParams', '$translate', '$timeout'];
