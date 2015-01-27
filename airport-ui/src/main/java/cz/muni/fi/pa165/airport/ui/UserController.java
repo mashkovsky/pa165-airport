890b0ce8443175ffa13d5ef6aa02152ed740b2c2
Jan Jílek <jan.jilek@freshflow.cz>
2015-01-27 14:36:32 +0100

    rest create user controller

--- airport-ui/src/main/java/cz/muni/fi/pa165/airport/ui/UserController.java
+++ airport-ui/src/main/java/cz/muni/fi/pa165/airport/ui/UserController.java
@@ -2,11 +2,14 @@ package cz.muni.fi.pa165.airport.ui;
 
 import cz.muni.fi.pa165.airport.api.dto.UserDTO;
 import cz.muni.fi.pa165.airport.api.security.CustomRole;
+import cz.muni.fi.pa165.airport.api.service.IUserService;
 import cz.muni.fi.pa165.airport.service.service.SecurityUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,11 +22,20 @@ public class UserController {
     @Autowired
     @Qualifier("loggedUser")
     private SecurityUser user;
+    
+    @Autowired
+    private IUserService userService;
 
     @PreAuthorize("hasRole('" + CustomRole.LOGGED + "')")
     @RequestMapping
     public UserDTO create() {
         return user.getUser();
+    }
+    
+    @RequestMapping(method = RequestMethod.POST, consumes = "application/json")
+    public UserDTO create(@RequestBody UserDTO user) {
+        userService.createUser(user);
+        return user;
     }
 
 }
